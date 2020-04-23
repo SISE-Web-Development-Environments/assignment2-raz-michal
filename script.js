1d35d3f06b827bc30475bf3829c2a1b0756796e9
razkl <tn5xjZhj>
2020-04-23 19:47:16 +0300

    before_critic_changes

--- script.js
+++ script.js
@@ -1,8 +1,74 @@
 // user-Details
 var database = new Array();
-
-// menu and login functions:
 database[0] = {username: "p", password: "p"};
+
+$(document).ready(function () {
+
+
+    // menu and login functions:
+    $("#btnRegister").click(function () {
+        var vfirstname = $("#firstname").val();
+        var vlastname = $("#lastname").val();
+        var vemail = $("#email").val();
+        var vpassword = $("#password").val();
+        var vusername = $("#username").val();
+        var vdate = $("#date").val();
+        if (vfirstname == '' || vlastname == '' || vemail == '' || vpassword == '' || vusername == '' || vdate == '') {
+            alert("Please fill all fields!");
+        } else if (vpassword.length < 6) {
+            alert("Password should have at least 6 characters!");
+        } else if (!(vpassword.match(/([a-zA-Z])/) && vpassword.match(/([0-9])/))) {
+            alert("Password must contain english letters and numbers");
+        } else if (vfirstname.match(/([0-9])/) || vlastname.match(/[0-9]/)) {
+            alert("Your name must not contain numbers");
+        } else if (!vemail.match(/^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/)) {
+            alert("Your e-mail is invalid");
+        } else {
+            database[database.length] = {username: vusername, password: vpassword};
+            document.getElementById('username').value = null;
+            document.getElementById('password').value = null;
+            var e2 = document.getElementById("register");
+            e2.style.display = 'none';
+            var e3 = document.getElementById("login");
+            e3.style.display = 'block';
+        }
+    });
+
+    $("#btnLogin").click(function () {
+        var boolean = false;
+        var thisuser = $("#loguser").val();
+        var thispassword = $("#logpassword").val();
+        for (var i = 0; i < database.length; i++) {
+            if (database[i].username == thisuser && database[i].password == thispassword) {
+                boolean = true;
+            }
+        }
+
+        if (boolean == false) {
+            alert("Wrong details. Please try again");
+        } else {
+            var e3 = document.getElementById("login");
+            e3.style.display = 'none';
+            var e4 = document.getElementById("gamewindow");
+            e4.style.display = 'block';
+        }
+    });
+
+    /*
+    var model = document.getElementById("myModal");
+    window.onclick = function (event) {
+        if (event.target == model) {
+            show_only_welcome()
+        }
+    }
+
+    var span = document.getElementsByClassName("close")[0];
+    span.onclick = function () {
+        show_only_welcome()
+    }
+    */
+
+});
 
 function show_only_welcome() {
     var e1 = document.getElementById("welcome");
@@ -33,74 +99,6 @@ function show_only_login() {
     e3.style.display = 'block';
 }
 
-$(document).ready(function () {
-    $("#btnRegister").click(function () {
-        var firstname = $("#firstname").val();
-        var lastname = $("#lastname").val();
-        var email = $("#email").val();
-        var password = $("#password").val();
-        var username = $("#username").val();
-        var date = $("#date").val();
-        if (firstname == '' || lastname == '' || email == '' || password == '' || username == '' || date == '') {
-            alert("Please fill all fields!");
-        } else if (password.length < 6) {
-            alert("Password should have at least 6 characters!");
-        } else if (!(password.match(/([a-zA-Z])/) && password.match(/([0-9])/))) {
-            alert("Password must contain english letters and numbers");
-        } else if (firstname.match(/([0-9])/) || lastname.match(/[0-9]/)) {
-            alert("Your name must not contain numbers");
-        } else if (!email.match(/^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/)) {
-            alert("Your e-mail is invalid");
-        } else {
-            database[database.length] = {username: username, password: password};
-            document.getElementById('username').value = null;
-            document.getElementById('password').value = null;
-            var e2 = document.getElementById("register");
-            e2.style.display = 'none';
-            var e3 = document.getElementById("login");
-            e3.style.display = 'block';
-        }
-    });
-});
-
-$(document).ready(function () {
-    $("#btnLogin").click(function () {
-        var boolean = false;
-        var thisuser = $("#username").val();
-        var thispassword = $("#password").val();
-
-        for (var i = 0; i < database.length; i++) {
-            if (database[i].username == thisuser && database[i].password == thispassword) {
-                boolean = true;
-            }
-        }
-
-        if (boolean == false) {
-            alert("Wrong details. Please try again");
-        } else {
-            var e3 = document.getElementById("login");
-            e3.style.display = 'none';
-            var e4 = document.getElementById("gamewindow");
-            e4.style.display = 'block';
-        }
-    });
-});
-
-
-$(document).ready(function () {
-    // When the user clicks anywhere outside of the modal, close it
-    var model = document.getElementById("myModal");
-    window.onclick = function (event) {
-        if (event.target == model) {
-            show_only_welcome()
-        }
-    }
-    var span = document.getElementsByClassName("close")[0];
-    span.onclick = function () {
-        show_only_welcome()
-    }
-});
-
 function show_about() {
     var e1 = document.getElementById("welcome");
     e1.style.display = 'none';
@@ -111,6 +109,8 @@ function show_about() {
     var e4 = document.getElementById("myModal");
     e4.style.display = 'block';
 }
+
+
 
 
 
